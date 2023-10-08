fa81ceba095502dded16972416f8bf91f2a5ce5c
generic-keith <keith.staines.ext@bbc.co.uk>
2023-10-08 09:00:40 +0100

    Initial Commit

---
+++ CoordinatorUI/CoordinatorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorUIApp.swift
+//  CoordinatorUI
+//
+//  Created by Keith Staines on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
